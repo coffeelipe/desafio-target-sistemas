d5b87ff27dab1547a87e5f18e48a11b23161a144
Felipe Pontes <fpontes321@gmail.com>
2026-04-14 19:52:34 -0300

    Rename the AppPalette class for consistency

--- app/lib/src/core/theme/app_palette.dart
+++ app/lib/src/core/theme/app_palette.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Palette {
+class AppPalette {
   static const scaffoldColor = Color(0xFFFEF5E6);
   static const primary = Color(0xFFD84214);
   static const secondary = Color(0xFFBA5C41);
