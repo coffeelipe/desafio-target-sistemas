f006ee34013d656092c42dc481a1e3e19bf6a24d
Felipe Pontes <fpontes321@gmail.com>
2026-04-14 19:53:13 -0300

    Define the scaffold color and default font in the app theme

--- app/lib/src/core/theme/app_theme.dart
+++ app/lib/src/core/theme/app_theme.dart
@@ -1,7 +1,11 @@
+import 'package:app/src/core/theme/app_palette.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final lightTheme = ThemeData.light().copyWith();
+  static final lightTheme = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: AppPalette.scaffoldColor,
+    textTheme: ThemeData.light().textTheme.apply(fontFamily: 'Poppins'),
+  );
 
   static final darkTheme = ThemeData.dark().copyWith();
 }
