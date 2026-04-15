adb6f5da9ab2db1b7dd53c05433fbdf0ac705a87
Felipe Pontes <fpontes321@gmail.com>
2026-04-15 16:02:54 -0300

    Remove unused import

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:app/src/core/theme/app_theme.dart';
 import 'package:app/src/pages/authentication/login_page.dart';
 import 'package:app/src/pages/authentication/registration_page.dart';
-import 'package:app/src/pages/home/home_page.dart';
 import 'package:app/src/pages/main_scaffold/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
