7e17d453a9f5f774d904218c9b027335e79d132a
Felipe Pontes <fpontes321@gmail.com>
2026-04-14 21:08:35 -0300

    Create responsive utils class for consistent spacing and responsive sizing

---
+++ app/lib/src/core/utils/responsive_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveUtils {
+  MediaQueryData mediaQuery(BuildContext context) => MediaQuery.of(context);
+
+  // breakpoint for tablet layout
+  bool isTablet(BuildContext context) => mediaQuery(context).size.width >= 600;
+
+  static double spacing(SpacingSize size) {
+    switch (size) {
+      case SpacingSize.small:
+        return 16.0;
+      case SpacingSize.medium:
+        return 24.0;
+      case SpacingSize.large:
+        return 32.0;
+    }
+  }
+}
+
+enum SpacingSize { small, medium, large }
