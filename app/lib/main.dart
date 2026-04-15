c5a29ac96aac27f00aa887edd5c5d46008ec4df0
Felipe Pontes <fpontes321@gmail.com>
2026-04-15 12:25:59 -0300

    Change the root route to point to the main scaffold

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app/src/core/theme/app_theme.dart';
 import 'package:app/src/pages/authentication/login_page.dart';
 import 'package:app/src/pages/authentication/registration_page.dart';
 import 'package:app/src/pages/home/home_page.dart';
+import 'package:app/src/pages/main_scaffold/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -36,7 +37,7 @@ class MainApp extends StatelessWidget {
             ];
           },
           routes: {
-            '/': (_) => const HomePage(),
+            '/': (_) => const MainScaffold(),
             '/login': (_) => const LoginPage(),
             '/register': (_) => const RegistrationPage(),
           },
