f5c190c6401984d21759ca80cd913a3676d057dc
Felipe Pontes <fpontes321@gmail.com>
2026-04-15 15:57:59 -0300

    Implement a store for the PageView state in the main scaffold

---
+++ app/lib/src/pages/main_scaffold/stores/pageview_state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+part 'pageview_state.g.dart';
+
+class PageViewState = _PageViewStateBase with _$PageViewState;
+
+abstract class _PageViewStateBase with Store {
+  final PageController controller = PageController(initialPage: 0);
+
+  @observable
+  int pageIndex = 0;
+
+  @action
+  void setIndex(int value) => pageIndex = value;
+
+  void dispose() => controller.dispose();
+}
