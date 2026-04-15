c6c776e726206fe0f5b8ca4beb4dd04a24d112ea
Felipe Pontes <fpontes321@gmail.com>
2026-04-14 21:05:37 -0300

    Add new colors to the app palette

--- app/lib/src/core/theme/app_palette.dart
+++ app/lib/src/core/theme/app_palette.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AppPalette {
   static const scaffoldColor = Color(0xFFFEF5E6);
   static const primary = Color(0xFFD84214);
+  static const primaryVariant = Color(0xFFAD362D);
   static const secondary = Color(0xFFBA5C41);
+  static const caption = Color(0xFF5E1703);
   static const tertiary = Color(0xFF0076E3);
 }
