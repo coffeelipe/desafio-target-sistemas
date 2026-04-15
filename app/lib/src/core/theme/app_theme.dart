e1248a33bcd668bbc605e6f458d5366230ff085e
Felipe Pontes <fpontes321@gmail.com>
2026-04-14 21:07:25 -0300

    Define the default input border in the app theme

--- app/lib/src/core/theme/app_theme.dart
+++ app/lib/src/core/theme/app_theme.dart
@@ -2,6 +2,15 @@ import 'package:app/src/core/theme/app_palette.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const OutlineInputBorder _inputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide.none,
+  );
+  static const OutlineInputBorder _errorInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(color: AppPalette.primary),
+  );
+
   static final lightTheme = ThemeData.light().copyWith(
     scaffoldBackgroundColor: AppPalette.scaffoldColor,
     textTheme: ThemeData.light().textTheme.apply(fontFamily: 'Poppins'),
