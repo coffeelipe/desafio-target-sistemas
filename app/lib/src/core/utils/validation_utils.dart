36e68e8b04af629965f0a4105c20e431582c892b
Felipe Pontes <fpontes321@gmail.com>
2026-04-16 15:49:25 -0300

    Implement validation logic for the username field in the registration page

---
+++ app/lib/src/core/utils/validation_utils.dart
@@ -0,0 +1,38 @@
+import 'package:app/src/core/error/validation_errors.dart';
+
+class ValidationUtils {
+  static String computeErrorMessage(FieldType fieldType, String value) {
+    switch (fieldType) {
+      case FieldType.username:
+        return _computeUsernameErrorMessage(value);
+      default:
+        return 'Algo deu errado';
+    }
+  }
+
+  static String _computeUsernameErrorMessage(String value) {
+    if (value.length < 3) {
+      return RegistrationValidationError
+          .usernameErrorMessages[UsernameValidationErrors
+          .insufficientCharacters]!;
+    } else if (RegExp(r'\s').hasMatch(value)) {
+      return RegistrationValidationError
+          .usernameErrorMessages[UsernameValidationErrors.containsSpace]!;
+    } else if (RegExp(r'[^a-zA-Z0-9_-]').hasMatch(value)) {
+      return RegistrationValidationError
+          .usernameErrorMessages[UsernameValidationErrors.invalidCharacters]!;
+    } else if (RegExp(r'^[0-9]').hasMatch(value)) {
+      return RegistrationValidationError
+          .usernameErrorMessages[UsernameValidationErrors.startsWithNumber]!;
+    } else if (RegExp(r'([-_]{2,})').hasMatch(value)) {
+      return RegistrationValidationError
+          .usernameErrorMessages[UsernameValidationErrors
+          .sequentialNonAlfaNumeric]!;
+    } else {
+      return RegistrationValidationError
+          .usernameErrorMessages[UsernameValidationErrors.invalidStartOrEnd]!;
+    }
+  }
+}
+
+enum FieldType { username, email, password }
