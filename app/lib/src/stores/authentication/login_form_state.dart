390801c8dad185fa802bf0b30054ce962b2c95a3
Felipe Pontes <fpontes321@gmail.com>
2026-04-17 12:29:14 -0300

    Implement mobx store for the login form

---
+++ app/lib/src/stores/authentication/login_form_state.dart
@@ -0,0 +1,43 @@
+
+import 'package:app/src/core/error/validation_errors.dart';
+import 'package:app/src/core/utils/validation_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+part 'login_form_state.g.dart';
+
+// ignore: library_private_types_in_public_api
+class LoginFormState = _LoginFormStateBase with _$LoginFormState;
+
+abstract class _LoginFormStateBase with Store {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  TextEditingController get emailController => _emailController;
+  TextEditingController get passwordController => _passwordController;
+
+  @action
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @observable
+  bool isPasswordObscured = true;
+
+  @action
+  void togglePasswordVisibility() => isPasswordObscured = !isPasswordObscured;
+
+  // validator
+  @action
+  Map<FieldType, String?> validateLoginFields(String? email, String? password) {
+    return {
+      FieldType.email: (email == null || email.isEmpty)
+          ? GlobalValidationError.emptyFieldErrorMessage
+          : null,
+      FieldType.password: (password == null || password.isEmpty)
+          ? GlobalValidationError.emptyFieldErrorMessage
+          : null,
+    };
+  }
+}
---
+++ app/lib/src/stores/authentication/login_form_state.g.dart
@@ -0,0 +1,77 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_form_state.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$LoginFormState on _LoginFormStateBase, Store {
+  late final _$isPasswordObscuredAtom = Atom(
+    name: '_LoginFormStateBase.isPasswordObscured',
+    context: context,
+  );
+
+  @override
+  bool get isPasswordObscured {
+    _$isPasswordObscuredAtom.reportRead();
+    return super.isPasswordObscured;
+  }
+
+  @override
+  set isPasswordObscured(bool value) {
+    _$isPasswordObscuredAtom.reportWrite(value, super.isPasswordObscured, () {
+      super.isPasswordObscured = value;
+    });
+  }
+
+  late final _$_LoginFormStateBaseActionController = ActionController(
+    name: '_LoginFormStateBase',
+    context: context,
+  );
+
+  @override
+  void dispose() {
+    final _$actionInfo = _$_LoginFormStateBaseActionController.startAction(
+      name: '_LoginFormStateBase.dispose',
+    );
+    try {
+      return super.dispose();
+    } finally {
+      _$_LoginFormStateBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void togglePasswordVisibility() {
+    final _$actionInfo = _$_LoginFormStateBaseActionController.startAction(
+      name: '_LoginFormStateBase.togglePasswordVisibility',
+    );
+    try {
+      return super.togglePasswordVisibility();
+    } finally {
+      _$_LoginFormStateBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  Map<FieldType, String?> validateLoginFields(String? email, String? password) {
+    final _$actionInfo = _$_LoginFormStateBaseActionController.startAction(
+      name: '_LoginFormStateBase.validateLoginFields',
+    );
+    try {
+      return super.validateLoginFields(email, password);
+    } finally {
+      _$_LoginFormStateBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+isPasswordObscured: ${isPasswordObscured}
+    ''';
+  }
+}
