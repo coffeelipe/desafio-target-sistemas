943297fbdaf4da7025ab6329497a5c2dc520b229
Felipe Pontes <fpontes321@gmail.com>
2026-04-17 00:10:06 -0300

    Implement the assisted password field for useful information while validating passwords

---
+++ app/lib/src/widgets/authentication/assisted_password_field.dart
@@ -0,0 +1,51 @@
+import 'package:app/src/widgets/authentication/password_assistant.dart';
+import 'package:app/src/widgets/global/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class AssistedPasswordField extends StatelessWidget {
+  final TextEditingController controller;
+  final FocusNode? focusNode;
+  final bool showAssistant;
+  final bool isObscured;
+  final Widget? suffixIcon;
+  final double passwordStrength;
+  final void Function(String)? onChanged;
+  final String? Function(String? value)? validator;
+
+  const AssistedPasswordField({
+    super.key,
+    required this.controller,
+    this.focusNode,
+    this.showAssistant = false,
+    required this.isObscured,
+    this.suffixIcon,
+    required this.passwordStrength,
+    this.onChanged,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AnimatedSize(
+          duration: Durations.short3,
+          child: showAssistant
+              ? PasswordAssistant(passwordStrength: passwordStrength)
+              : const SizedBox(width: double.infinity, height: 0),
+        ),
+        const SizedBox(height: 8),
+        CustomTextFormField(
+          controller: controller,
+          focusNode: focusNode,
+          label: 'Senha',
+          hintText: 'Senha',
+          obscureText: isObscured,
+          suffixIcon: suffixIcon,
+          onChanged: onChanged,
+          validator: validator,
+        ),
+      ],
+    );
+  }
+}
