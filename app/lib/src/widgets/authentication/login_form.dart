48dc599d5cd21b7bdfb34b06a88a2d75447ef017
Felipe Pontes <fpontes321@gmail.com>
2026-04-17 12:37:52 -0300

    Implement login's form syntax validation

--- app/lib/src/widgets/authentication/login_form.dart
+++ app/lib/src/widgets/authentication/login_form.dart
@@ -1,50 +1,76 @@
 import 'package:app/src/core/utils/responsive_utils.dart';
+import 'package:app/src/core/utils/validation_utils.dart';
+import 'package:app/src/stores/authentication/login_form_state.dart';
+import 'package:app/src/widgets/authentication/password_field.dart';
 import 'package:app/src/widgets/global/custom_elevated_button.dart';
 import 'package:app/src/widgets/global/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          CustomTextFormField(
-            controller: TextEditingController(),
-            label: 'Email',
-            hintText: 'email@exemplo.com',
-          ),
-          SizedBox(height: ResponsiveUtils.spacing(SpacingSize.small)),
-          CustomTextFormField(
-            controller: TextEditingController(),
-            label: 'Senha',
-            hintText: 'Senha',
-          ),
-          SizedBox(height: ResponsiveUtils.spacing(SpacingSize.large)),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: ResponsiveUtils.spacing(SpacingSize.small),
-            ),
-            child: GradientButton(
-              onPressed: () => Navigator.pushNamedAndRemoveUntil(
-                context,
-                '/',
-                (route) => false,
+    final loginFormStateProvider = Provider.of<LoginFormState>(context);
+
+    return Observer(
+      builder: (context) {
+        final String emailInput = loginFormStateProvider.emailController.text;
+        final String passwordInput =
+            loginFormStateProvider.passwordController.text;
+
+        return Form(
+          key: loginFormStateProvider.loginFormKey,
+          child: Column(
+            children: [
+              CustomTextFormField(
+                controller: TextEditingController(),
+                label: 'Email',
+                hintText: 'email@exemplo.com',
+                validator: (value) => loginFormStateProvider
+                    .validateLoginFields(value, passwordInput)[FieldType.email],
               ),
-              child: const Text(
-                'Entrar',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+              SizedBox(height: ResponsiveUtils.spacing(SpacingSize.small)),
+              PasswordField(
+                controller: loginFormStateProvider.passwordController,
+                obscureText: loginFormStateProvider.isPasswordObscured,
+                onTogglePressed:
+                    loginFormStateProvider.togglePasswordVisibility,
+                validator: (value) => loginFormStateProvider
+                    .validateLoginFields(emailInput, value)[FieldType.password],
+              ),
+              SizedBox(height: ResponsiveUtils.spacing(SpacingSize.large)),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: ResponsiveUtils.spacing(SpacingSize.small),
+                ),
+                child: GradientButton(
+                  onPressed: () {
+                    if (loginFormStateProvider.loginFormKey.currentState!
+                        .validate()) {
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        '/',
+                        (route) => false,
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
