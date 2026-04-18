0ee38bf7c3973bc28d65408bb271d90708a19d9a
Felipe Pontes <fpontes321@gmail.com>
2026-04-18 13:46:10 -0300

    Remove safe area from main scaffold for better edge to edge design

--- app/lib/src/pages/main_scaffold/main_scaffold.dart
+++ app/lib/src/pages/main_scaffold/main_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:app/src/core/extensions/mediaquery_extension.dart';
+import 'package:app/src/core/theme/app_palette.dart';
 import 'package:app/src/pages/dashboard/dashboard.dart';
 import 'package:app/src/pages/home/home_page.dart';
 import 'package:app/src/pages/profile/profile.dart';
@@ -30,34 +31,43 @@ class _MainScaffoldState extends State<MainScaffold> {
       builder: (_) {
         final int pageIndex = _mainScaffoldStore.pageViewState.pageIndex;
         return Scaffold(
-          body: SafeArea(
-            child: Stack(
-              children: [
-                PageView(
-                  controller: _mainScaffoldStore.pageViewState.controller,
-                  onPageChanged: _mainScaffoldStore.pageViewState.onPageChanged,
-                  children: [
-                    Provider.value(
-                      value: _mainScaffoldStore.homeStore,
-                      child: const HomePage(),
-                    ),
-                    const Dashboard(),
-                    const Profile(),
-                  ],
-                ),
-                Positioned(
-                  bottom: context.safeBottom + 32,
-                  left: 0,
-                  right: 0,
-                  child: CustomNavBar(
-                    pageController: _mainScaffoldStore.pageViewState.controller,
-                    onDestinationSelected: _mainScaffoldStore.pageViewState
-                        .onDestinationSelected(pageIndex),
-                    selectedIndex: pageIndex,
+          body: Stack(
+            children: [
+              PageView(
+                controller: _mainScaffoldStore.pageViewState.controller,
+                onPageChanged: _mainScaffoldStore.pageViewState.onPageChanged,
+                children: [
+                  Provider.value(
+                    value: _mainScaffoldStore.homeStore,
+                    child: const HomePage(),
+                  ),
+                  const Dashboard(),
+                  const Profile(),
+                ],
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: context.mediaQuery.padding.top + context.safeTop,
+                  decoration: BoxDecoration(
+                    gradient: AppPalette.edgeToEdgeGradient,
                   ),
                 ),
-              ],
-            ),
+              ),
+              Positioned(
+                bottom: context.safeBottom + 32,
+                left: 0,
+                right: 0,
+                child: CustomNavBar(
+                  pageController: _mainScaffoldStore.pageViewState.controller,
+                  onDestinationSelected: _mainScaffoldStore.pageViewState
+                      .onDestinationSelected(pageIndex),
+                  selectedIndex: pageIndex,
+                ),
+              ),
+            ],
           ),
         );
       },
