87dd0b23982c461ac879c167d547ea81aa10875f
Felipe Pontes <fpontes321@gmail.com>
2026-04-17 22:34:25 -0300

    Implement the signin method in the auth service

--- app/lib/src/services/authentication/auth_service.dart
+++ app/lib/src/services/authentication/auth_service.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  Future<UserCredential> registerUser(
+  static const String _serviceTag = '[AUTH_SERVICE]';
+
+  Future<UserCredential?> registerUser(
     String email,
     String password,
     String username,
@@ -14,7 +16,31 @@ class AuthService {
 
       return credential;
     } on FirebaseAuthException catch (e) {
-      throw Exception('Caught exception during registration: $e');
+      print('$_serviceTag Caught exception during registration: $e');
+      return null;
+    }
+  }
+
+  Future<UserCredential?> loginWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final UserCredential credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      print('$_serviceTag Caught exception during login: $e');
+      return null;
+    }
+  }
+
+  Future<User?> getCurrentUser() async {
+    try {
+      return FirebaseAuth.instance.currentUser;
+    } on FirebaseAuthException catch (e) {
+      print('$_serviceTag Caught exception while fetching current user $e');
+      return null;
     }
   }
 }
