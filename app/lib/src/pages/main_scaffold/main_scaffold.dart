4ee93a252536e497be5829ae46c899ccaea67a64
Felipe Pontes <fpontes321@gmail.com>
2026-04-18 01:03:52 -0300

    Wrap main scaffold in safe area

--- app/lib/src/pages/main_scaffold/main_scaffold.dart
+++ app/lib/src/pages/main_scaffold/main_scaffold.dart
@@ -1,6 +1,7 @@
 import 'package:app/src/core/extensions/mediaquery_extension.dart';
 import 'package:app/src/pages/dashboard/dashboard.dart';
 import 'package:app/src/pages/home/home_page.dart';
+import 'package:app/src/pages/profile/profile.dart';
 import 'package:app/src/stores/main_scaffold/main_scaffold_store.dart';
 import 'package:app/src/widgets/main_scaffold/custom_nav_bar.dart';
 import 'package:flutter/material.dart';
@@ -28,31 +29,34 @@ class _MainScaffoldState extends State<MainScaffold> {
       builder: (_) {
         final int pageIndex = _mainScaffoldStore.pageViewState.pageIndex;
         return Scaffold(
-          body: Stack(
-            children: [
-              PageView(
-                controller: _mainScaffoldStore.pageViewState.controller,
-                onPageChanged: (value) => _mainScaffoldStore.pageViewState.setIndex(value),
-                children: const [HomePage(), Dashboard()],
-              ),
-              Positioned(
-                bottom: context.safeBottom + 32,
-                left: 0,
-                right: 0,
-                child: CustomNavBar(
-                  pageController: _mainScaffoldStore.pageViewState.controller,
-                  onDestinationSelected: (value) {
-                    _mainScaffoldStore.pageViewState.setIndex(value);
-                    _mainScaffoldStore.pageViewState.controller.animateToPage(
-                      _mainScaffoldStore.pageViewState.pageIndex,
-                      duration: Durations.short3,
-                      curve: Curves.easeInOut,
-                    );
-                  },
-                  selectedIndex: pageIndex,
+          body: SafeArea(
+            child: Stack(
+              children: [
+                PageView(
+                  controller: _mainScaffoldStore.pageViewState.controller,
+                  onPageChanged: (value) =>
+                      _mainScaffoldStore.pageViewState.setIndex(value),
+                  children: const [HomePage(), Dashboard(), Profile()],
                 ),
-              ),
-            ],
+                Positioned(
+                  bottom: context.safeBottom + 32,
+                  left: 0,
+                  right: 0,
+                  child: CustomNavBar(
+                    pageController: _mainScaffoldStore.pageViewState.controller,
+                    onDestinationSelected: (value) {
+                      _mainScaffoldStore.pageViewState.setIndex(value);
+                      _mainScaffoldStore.pageViewState.controller.animateToPage(
+                        _mainScaffoldStore.pageViewState.pageIndex,
+                        duration: Durations.short3,
+                        curve: Curves.easeInOut,
+                      );
+                    },
+                    selectedIndex: pageIndex,
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
