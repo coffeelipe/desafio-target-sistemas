ca2377529eff5ddb149e0b02421006b7ba16c795
Felipe Pontes <fpontes321@gmail.com>
2026-04-18 00:08:19 -0300

    Implement a splash screen widget

---
+++ app/lib/src/pages/splash/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:app/src/core/extensions/mediaquery_extension.dart';
+import 'package:app/src/core/theme/app_palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
+                height: context.mediaQuery.size.height * 0.08,
+              ),
+            ),
+            const SpinKitThreeBounce(color: AppPalette.primary, size: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
