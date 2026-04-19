fcf427ecf457803178e5655c81a6cf9640d1cd52
Felipe Pontes <fpontes321@gmail.com>
2026-04-19 07:58:16 -0300

    Link profile store to the root store

--- app/lib/src/stores/main/root_store.dart
+++ app/lib/src/stores/main/root_store.dart
@@ -1,6 +1,7 @@
 import 'package:app/src/stores/authentication/auth_store.dart';
 import 'package:app/src/stores/authentication/user_profile_store.dart';
 import 'package:app/src/stores/home/note_store.dart';
+import 'package:app/src/stores/profile/profile_store.dart';
 import 'package:mobx/mobx.dart';
 part 'root_store.g.dart';
 
@@ -11,14 +12,17 @@ abstract class _RootStoreBase with Store {
   late final NoteStore noteStore;
   late final UserProfileStore userProfileStore;
   late final AuthStore authStore;
+  late final ProfileStore profileStore;
 
   _RootStoreBase() {
     noteStore = NoteStore(root: this as RootStore);
     userProfileStore = UserProfileStore(root: this as RootStore);
     authStore = AuthStore(root: this as RootStore);
+    profileStore = ProfileStore(root: this as RootStore);
   }
 
   void dispose() {
     authStore.dispose();
+    profileStore.dispose();
   }
 }
