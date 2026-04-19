3d0d0c9b406df4bde55122b421a8eda89fee5e1b
Felipe Pontes <fpontes321@gmail.com>
2026-04-19 02:52:12 -0300

    Remove the literal r from the emoji regex validator pattern

--- app/lib/src/core/error/validation_patterns.dart
+++ app/lib/src/core/error/validation_patterns.dart
@@ -9,6 +9,6 @@ class ValidationPatterns {
   );
   static final RegExp emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
   static final RegExp emojiRegex = RegExp(
-    r'([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF]|[\\r])',
+    r'([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])',
   );
 }
