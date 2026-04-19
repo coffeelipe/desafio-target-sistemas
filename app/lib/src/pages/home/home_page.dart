9df0f0519d1d47cedcc2f7a9e4d292b04056b2df
Felipe Pontes <fpontes321@gmail.com>
2026-04-18 23:40:19 -0300

    Implement the gridview with thumbnails

--- app/lib/src/pages/home/home_page.dart
+++ app/lib/src/pages/home/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:app/src/core/extensions/mediaquery_extension.dart';
 import 'package:app/src/core/utils/responsive_utils.dart';
+import 'package:app/src/pages/fullscreen_note/fullscreen_note.dart';
 import 'package:app/src/stores/home/home_store.dart';
 import 'package:app/src/stores/main/root_store.dart';
+import 'package:app/src/widgets/global/empty_state.dart';
 import 'package:app/src/widgets/home/header.dart';
+import 'package:app/src/widgets/home/note/note_thumbnail.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
+import 'package:remixicon/remixicon.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,10 +20,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late final HomeStore _homeStore;
+  late final RootStore _rootStore;
+
   @override
   void initState() {
     super.initState();
     _homeStore = context.read<HomeStore>();
+    _rootStore = context.read<RootStore>();
     _homeStore.setGreetingDisplayName(
       context.read<RootStore>().userProfileStore.displayName!,
     );
@@ -26,37 +34,69 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<UserScrollNotification>(
-      onNotification: _homeStore.onScrollNotification(),
-      child: ListView.builder(
-        controller: mounted ? _homeStore.scrollController : null,
-        itemCount: 50,
-        padding: EdgeInsets.fromLTRB(
-          ResponsiveUtils.spacing(SpacingSize.large),
-          ResponsiveUtils.spacing(SpacingSize.small) + context.safeTop,
-          ResponsiveUtils.spacing(SpacingSize.large),
-          ResponsiveUtils.spacing(SpacingSize.small) + context.safeBottom,
-        ),
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Header(greetingMessage: _homeStore.greetingMessage);
-          }
-          return Container(
-            height: 100,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 5,
-                  offset: Offset(0, 5),
-                ),
+    return Observer(
+      builder: (_) {
+        return NotificationListener<UserScrollNotification>(
+          onNotification: _homeStore.onScrollNotification(),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+              ResponsiveUtils.spacing(SpacingSize.large),
+              ResponsiveUtils.spacing(SpacingSize.small) + context.safeTop,
+              ResponsiveUtils.spacing(SpacingSize.large),
+              ResponsiveUtils.spacing(SpacingSize.small) + context.safeBottom,
+            ),
+            child: Column(
+              children: [
+                Header(greetingMessage: _homeStore.greetingMessage),
+                _rootStore.noteStore.notes.isEmpty
+                    ? Expanded(
+                        child: Center(
+                          child: EmptyState(
+                            icon: RemixIcons.sticky_note_fill,
+                            message: 'Nenhuma nota encontrada',
+                            ctaText: 'Criar minha primeira nota',
+                            onCtaPressed: () => _rootStore.noteStore
+                                .showNewNoteDialog(context, null),
+                          ),
+                        ),
+                      )
+                    : Expanded(
+                        child: GridView.builder(
+                          controller: _homeStore.scrollController,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: ResponsiveUtils.spacing(
+                                  SpacingSize.small,
+                                ),
+                                crossAxisSpacing: ResponsiveUtils.spacing(
+                                  SpacingSize.small,
+                                ),
+                                childAspectRatio: 0.75,
+                              ),
+                          itemCount: _rootStore.noteStore.notes.length,
+                          itemBuilder: (context, index) {
+                            return NoteThumbnail(
+                              index: index,
+                              onTap: () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return FullscreenNote(
+                                      note: _rootStore.noteStore.notes[index],
+                                    );
+                                  },
+                                ),
+                              ),
+                              note: _rootStore.noteStore.notes[index],
+                            );
+                          },
+                        ),
+                      ),
               ],
             ),
-            margin: const EdgeInsets.symmetric(vertical: 8),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
