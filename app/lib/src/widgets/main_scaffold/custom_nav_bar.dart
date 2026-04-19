eaf3d5422fa6cf3ea095a5fb132f03bb4096d98e
Felipe Pontes <fpontes321@gmail.com>
2026-04-19 11:00:54 -0300

    Remove stack height

--- app/lib/src/widgets/main_scaffold/custom_nav_bar.dart
+++ app/lib/src/widgets/main_scaffold/custom_nav_bar.dart
@@ -1,4 +1,3 @@
-import 'package:app/src/core/extensions/mediaquery_extension.dart';
 import 'package:app/src/core/theme/app_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:remixicon/remixicon.dart';
@@ -20,75 +19,73 @@ class CustomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double navBarStackHeight = 130;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: SizedBox(
-        height: context.screenHeight * 0.3,
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: const [
-                    BoxShadow(
-                      offset: Offset(0, 5),
-                      color: Colors.black26,
-                      blurRadius: 8,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 5),
+                    color: Colors.black26,
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 6),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildNavButton(
+                      onPressed: onDestinationSelected,
+                      index: 0,
+                    ),
+                    _buildNavButton(
+                      onPressed: onDestinationSelected,
+                      index: 1,
+                    ),
+                    _buildNavButton(
+                      onPressed: onDestinationSelected,
+                      index: 2,
                     ),
                   ],
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 6),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      _buildNavButton(
-                        onPressed: onDestinationSelected,
-                        index: 0,
-                      ),
-                      _buildNavButton(
-                        onPressed: onDestinationSelected,
-                        index: 1,
-                      ),
-                      _buildNavButton(
-                        onPressed: onDestinationSelected,
-                        index: 2,
-                      ),
-                    ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: -15,
+            bottom: 70,
+            child: AnimatedRotation(
+              turns: isFABVisible ? 0 : -0.25,
+              duration: Durations.short3,
+              child: AnimatedScale(
+                duration: Durations.short4,
+                scale: isFABVisible ? 1 : 0,
+                child: FloatingActionButton(
+                  onPressed: isFABVisible
+                      ? () => onFabPressed(context)
+                      : null,
+                  shape: const CircleBorder(),
+                  backgroundColor: AppPalette.primary,
+                  child: const Icon(
+                    RemixIcons.sticky_note_add_line,
+                    color: Colors.white,
+                    size: 32,
                   ),
                 ),
               ),
             ),
-            Positioned(
-              right: -15,
-              bottom: 70,
-              child: AnimatedRotation(
-                turns: isFABVisible ? 0 : -0.25,
-                duration: Durations.short3,
-                child: AnimatedScale(
-                  duration: Durations.short4,
-                  scale: isFABVisible ? 1 : 0,
-                  child: FloatingActionButton(
-                    onPressed: isFABVisible
-                        ? () => onFabPressed(context)
-                        : null,
-                    shape: const CircleBorder(),
-                    backgroundColor: AppPalette.primary,
-                    child: const Icon(
-                      RemixIcons.sticky_note_add_line,
-                      color: Colors.white,
-                      size: 32,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
