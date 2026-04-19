35b8034541a7f9c6e867105f10623b2a77cdfb58
Felipe Pontes <fpontes321@gmail.com>
2026-04-19 02:40:22 -0300

    Implement the progress bar undo button

--- app/lib/src/widgets/home/thumb_options.dart
+++ app/lib/src/widgets/home/thumb_options.dart
@@ -1,3 +1,4 @@
+import 'package:app/src/core/theme/app_palette.dart';
 import 'package:app/src/models/note.dart';
 import 'package:flutter/material.dart';
 import 'package:remixicon/remixicon.dart';
@@ -6,22 +7,49 @@ class ThumbOptions extends StatelessWidget {
   final Note note;
   final VoidCallback? onEdit;
   final VoidCallback? onDelete;
+  final bool isMarkedForDeletion;
+  final double? progress;
   const ThumbOptions({
     super.key,
     required this.note,
     this.onEdit,
     this.onDelete,
+    this.isMarkedForDeletion = false,
+    this.progress,
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisSize: MainAxisSize.min,
       children: [
         _buildActionButton(onPressed: onEdit, icon: RemixIcons.pencil_line),
-        _buildActionButton(
-          onPressed: onDelete,
-          icon: RemixIcons.delete_bin_line,
+        SizedBox.square(
+          dimension: 32,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              if (onDelete != null && progress != null)
+                Positioned.fill(
+                  child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: CircularProgressIndicator(
+                      value: progress!.clamp(0, 1),
+                      color: AppPalette.primary,
+                      strokeWidth: 2,
+                    ),
+                  ),
+                ),
+              _buildActionButton(
+                onPressed: onDelete,
+                icon: isMarkedForDeletion
+                    ? RemixIcons.arrow_go_back_line
+                    : RemixIcons.delete_bin_line,
+                color: AppPalette.primary,
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -30,6 +58,7 @@ class ThumbOptions extends StatelessWidget {
   IconButton _buildActionButton({
     required VoidCallback? onPressed,
     required IconData icon,
+    Color? color,
   }) {
     return IconButton(
       onPressed: onPressed,
@@ -37,7 +66,8 @@ class ThumbOptions extends StatelessWidget {
       visualDensity: VisualDensity.compact,
       constraints: const BoxConstraints(minWidth: 36, minHeight: 36),
       splashRadius: 18,
-      icon: Icon(icon, size: 20),
+      iconSize: 20,
+      icon: Icon(icon, color: color),
     );
   }
 }
