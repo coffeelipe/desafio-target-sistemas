83c4332cd3ddd4de01972ffb7d1a4b73792498bc
Felipe Pontes <fpontes321@gmail.com>
2026-04-18 23:37:37 -0300

    Implement fromContent factory for the note model

--- app/lib/src/models/note.dart
+++ app/lib/src/models/note.dart
@@ -20,4 +20,31 @@ class Note {
     required this.totalDigits,
     required this.totalSpecialCharacters,
   });
+
+  factory Note.fromContent({
+    required String id,
+    String? title,
+    required String content,
+  }) {
+    final lines = content.split('\n');
+    final words = content.trim().split(RegExp(r'\s+'));
+    final characters = content.length;
+    final letters = content.replaceAll(RegExp(r'[^A-Za-z]'), '').length;
+    final digits = content.replaceAll(RegExp(r'[^0-9]'), '').length;
+    final specialCharacters = content
+        .replaceAll(RegExp(r'[A-Za-z0-9\s]'), '')
+        .length;
+
+    return Note(
+      id: id,
+      title: title,
+      content: content,
+      numberOfLines: lines.length,
+      numberOfWords: words.length,
+      totalCharacters: characters,
+      totalLetters: letters,
+      totalDigits: digits,
+      totalSpecialCharacters: specialCharacters,
+    );
+  }
 }
